a8cd190686647e160d41b21532c0567fa07a880e
WilliamChung <williamc825@hotmail.com>
2016-09-29 15:09:47 -0700

    Initial Commit

---
+++ iProfile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iProfile
+//
+//  Created by T00037035 on 2016-09-29.
+//  Copyright © 2016 WilliamChung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
