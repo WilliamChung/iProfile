f2b817049c713d575bd7cdc9ad3b921f28adeb52
WilliamChung <williamc825@hotmail.com>
2016-10-12 14:03:52 -0700

    added layout
    keyboard but no functions yet

--- iProfile/ViewController.swift
+++ iProfile/ViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func hitLetter(_ sender: UIButton) {
+        print("q")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
+    
 
 
 }
